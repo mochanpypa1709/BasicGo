5ebec0527825036b7d877164a8d06a7f3135dece
mochanpypa1709 <65754475+mochanpypa1709@users.noreply.github.com>
2021-05-25 13:54:52 +0700

    Update main.go

--- main.go
+++ main.go
@@ -4,16 +4,10 @@ import "github.com/mochanpypa1709/BasicGo/loop"
 
 func main() {
 	//variable.Arguments()
-
 	//strings.RightTrim()
-
 	//iota.Seasons()
-
 	//ifstate.UserPass2()
-
 	//err_handling.Dayinmonth()
-
 	//switching.Cont()
-
 	loop.Inf()
 }
